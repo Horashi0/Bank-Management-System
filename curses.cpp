e9e179ac010936d8d3b10906b747ff11e937079f
Horashio <Horashi0@users.noreply.github.com>
2023-09-10 16:55:33 +0100

    Further improved menu bar
    Added items to menu bar

--- curses.cpp
+++ curses.cpp
@@ -1,25 +1,41 @@
 #include "stdio.h"
 #include "ncurses.h"
 #include "curses.hpp"
+#include <iostream>
 
 using namespace std;
 
 int main() {
-  initscr(); // Initialise NCURSES mode
+  initscr(); // Initialise ncurses mode
   noecho(); // Doesnt print out users input
   curs_set(0); // nvisible, normal, or very visible for visibility equal to 0, 1, or 2
 
+  // Check for terminal colour compatibility and create colours
+
+  if(!has_colors())
+  {
+    return -1;
+  }
+  start_color(); // Initialise ncurses colour mode
+  init_pair(1, COLOR_WHITE, COLOR_BLUE);
+
   // Window is half the size of the terminal and centered in the middle
   int yMax, xMax;
+
   getmaxyx(stdscr, yMax, xMax);
 
   WINDOW *win = newwin(yMax/2, xMax/2, yMax/4, xMax/4);
   box(win, 0, 0);
 
-  Menu menus[3] = {
-    Menu("File", 'f'),
-    Menu("Edit", 'e'),
-    Menu("Options", 'o'),
+  string menu1[] = {"New", "Open", "Save", "Exit"};
+  string menu2[] = {"Copy", "Cut", "Paste"};
+  string menu3[] = {"Sidebar", "Terminal"};
+
+  Menu menus[3] = 
+  {
+    Menu("File", 'f', menu1, 4),
+    Menu("Edit", 'e', menu2, 3),
+    Menu("Options", 'o', menu3, 2)
   };
 
   MenuBar menubar = MenuBar(win, menus, 3);
--- curses.hpp
+++ curses.hpp
@@ -10,27 +10,62 @@ class Menu
 {
     public:
 
-        Menu(string text, char trigger)
+        Menu(string text, char trigger, string *items, int num_items)
         {
             this->text = text;
             this->trigger = trigger;
+            this->items = items;
+            this->num_items = num_items;
+            this->selected_item = 0;
         }
 
         int start_x;
         string text;
         char trigger;
 
+        string *items;
+        int num_items;
+        int selected_item;
+
+        void selectNextItem()
+        {
+            selected_item++;
+            if(selected_item >= num_items)
+            {
+                selected_item = 0;
+            }
+        }
+
+        void selectPreviousItem()
+        {
+            selected_item--;
+            if(selected_item < 0)
+            {
+                selected_item = num_items - 1;
+            }
+        }
+
 };
 
 class MenuBar 
 {
     public:
-        MenuBar(WINDOW *win, Menu* menus, int num_menus)
+        MenuBar(WINDOW *win, Menu *menus, int num_menus)
         {
             this->win = win;
             this->menus = menus;
             this->num_menus = num_menus; 
             this->selected_menu = -1;
+            
+            // Declaring new window inside of win
+            int yMax, xMax, yBeg, xBeg;
+            getmaxyx(win, yMax, xMax);
+            getbegyx(win, yBeg, xBeg);
+            menuwin = newwin(yMax -2, xMax -2, yBeg + 1, xBeg + 1);
+            // Returns a value depending on what function key is pressed by user
+            keypad(menuwin, true); 
+            wrefresh(menuwin);
+
             int current_pos = 2;
 
             for(int i = 0; i < num_menus; i++)
@@ -40,20 +75,78 @@ class MenuBar
             }
         }
 
+        void reset()
+        {
+            for(int i = 0; i < num_menus; i++)
+            {
+                int start_x = menus[i].start_x;
+                string text = menus[i].text;
+                mvwprintw(win, 0, start_x, text.c_str());
+            }
+            wrefresh(win);
+        }
+
         void draw()
         {
             for(int i = 0; i < num_menus; i++)
             {
-                int start_x = this->menus[i].start_x;
-                string text = this->menus[i].text;
-                if(selected_menu == i)
+                drawMenu(menus[i], selected_menu == i);
+            }
+            selected_menu = -1;
+        }
+
+        void drawMenu(Menu menu, bool is_selected)
+        {
+            
+            int start_x = menu.start_x;
+            string text = menu.text;
+            if(is_selected)
+            {
+                wattron(win, A_STANDOUT);
+            }
+            mvwprintw(win, 0, start_x, text.c_str());
+            wattroff(win, A_STANDOUT);
+            wrefresh(win);
+
+            int ch;
+            drawMenuItems(menu);
+            wrefresh(menuwin);
+            while (is_selected && (ch = wgetch(menuwin)))
+            {
+                switch(ch)
                 {
-                    wattron(win, A_STANDOUT);
-                   
+                    case KEY_DOWN:
+                        menu.selectNextItem();
+                        break;
+                    case KEY_UP:
+                        menu.selectPreviousItem();
+                        break;
+                    default:
+                        is_selected = false;
                 }
-                mvwprintw(win, 0, start_x, text.c_str());
-                wattroff(win, A_STANDOUT);
-                
+                drawMenuItems(menu);
+            }
+            werase(menuwin);
+            wrefresh(menuwin);
+            reset();
+
+        }
+
+        void drawMenuItems(Menu menu)
+        {
+            int yMax, xMax;
+            getmaxyx(menuwin, yMax, xMax);
+            for(int i = 0; i < menu.num_items; i++)
+            {
+                mvwprintw(menuwin, i, 0, menu.items[i].c_str());
+                if(menu.selected_item == i)
+                {
+                    mvwchgat(menuwin, i, 0, xMax, A_NORMAL, 1, NULL);
+                }
+                else
+                {
+                    mvwchgat(menuwin, i, 0, xMax, A_STANDOUT, 0, NULL);
+                }
             }
         }
 
@@ -69,14 +162,9 @@ class MenuBar
             
         }
 
-
-
-
-
-
-
-        WINDOW * win;
-        Menu * menus;
+        WINDOW *menuwin;
+        WINDOW *win;
+        Menu *menus;
         int num_menus;
         int selected_menu;
 };
