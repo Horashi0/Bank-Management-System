b2f732fd8cdbcd4657b19b5d536d55cdf1970bb6
Horashio <Horashi0@users.noreply.github.com>
2023-09-12 10:24:08 +0100

    Progress commit
    Non working

--- curses.cpp
+++ curses.cpp
@@ -16,36 +16,39 @@ int main() {
   {
     return -1;
   }
+
   start_color(); // Initialise ncurses colour mode
   init_pair(1, COLOR_WHITE, COLOR_BLUE);
+  init_pair(2, COLOR_BLUE, COLOR_WHITE);
 
   // Window is half the size of the terminal and centered in the middle
   int yMax, xMax;
-
   getmaxyx(stdscr, yMax, xMax);
-
   WINDOW *win = newwin(yMax/2, xMax/2, yMax/4, xMax/4);
-  box(win, 0, 0);
 
-  string menu1[] = {"New", "Open", "Save", "Exit"};
-  string menu2[] = {"Copy", "Cut", "Paste"};
-  string menu3[] = {"Sidebar", "Terminal"};
+  wbkgd(win, COLOR_PAIR(2));
+  wbkgd(stdscr, COLOR_PAIR(1));
+  refresh();
 
-  Menu menus[3] = 
+  MenuBar menubar;
+  menubar.centralText(stdscr, "HORASHIO BANKING SYSTEM", 8);
+
+  Menu menus[4]
   {
-    Menu("File", 'f', menu1, 4),
-    Menu("Edit", 'e', menu2, 3),
-    Menu("Options", 'o', menu3, 2)
+    Menu("1. Create new account", '1'),
+    Menu("2. Log into existing account", '2'),
+    Menu("3. Update information on existing account", '3'),
+    Menu("4. Transfer money", '4'),
   };
-
-  MenuBar menubar = MenuBar(win, menus, 3);
-  menubar.draw();
+  string hello = "hello";
+  mvwprintw(win, 0, 0, hello.c_str());
+  mvwprintw(win, 1, 0, hello.c_str());
 
   char ch;
-  while (ch = wgetch(win))
+  while(ch = wgetch(win))
   {
-    menubar.handleTrigger(ch);
-    menubar.draw();
+    
   }
+  
   endwin(); // End ncurses mode
 }
--- curses.hpp
+++ curses.hpp
@@ -9,164 +9,57 @@ using namespace std;
 class Menu
 {
     public:
-
-        Menu(string text, char trigger, string *items, int num_items)
+        Menu(string text, char trigger)
         {
             this->text = text;
             this->trigger = trigger;
-            this->items = items;
-            this->num_items = num_items;
-            this->selected_item = 0;
         }
 
-        int start_x;
         string text;
-        char trigger;
-
-        string *items;
-        int num_items;
-        int selected_item;
-
-        void selectNextItem()
-        {
-            selected_item++;
-            if(selected_item >= num_items)
-            {
-                selected_item = 0;
-            }
-        }
-
-        void selectPreviousItem()
-        {
-            selected_item--;
-            if(selected_item < 0)
-            {
-                selected_item = num_items - 1;
-            }
-        }
-
+        char trigger;     
 };
 
-class MenuBar 
+class MenuBar
 {
     public:
-        MenuBar(WINDOW *win, Menu *menus, int num_menus)
+        MenuBar(WINDOW *win, Menu *menus, int numMenus)
         {
             this->win = win;
-            this->menus = menus;
-            this->num_menus = num_menus; 
-            this->selected_menu = -1;
+            this->menus = menus
+        }
+
+
+
+
+
+
+
+
+        void centralText(WINDOW *win, string text, int startRowDivider)
+        {
+            int startRow = win->_maxy / startRowDivider;
+            int centerCol = win->_maxx / 2;
+            int halfLength = text.length() / 2;
+            int centralised = centerCol - halfLength;
             
-            // Declaring new window inside of win
-            int yMax, xMax, yBeg, xBeg;
-            getmaxyx(win, yMax, xMax);
-            getbegyx(win, yBeg, xBeg);
-            menuwin = newwin(yMax -2, xMax -2, yBeg + 1, xBeg + 1);
-            // Returns a value depending on what function key is pressed by user
-            keypad(menuwin, true); 
-            wrefresh(menuwin);
-
-            int current_pos = 2;
-
-            for(int i = 0; i < num_menus; i++)
-            {
-                this->menus[i].start_x = current_pos;
-                current_pos += this->menus[i].text.length() + 1;
-            }
+            mvwprintw(win, startRow, centralised, text.c_str());
         }
 
-        void reset()
+        void draw(Menu menu, bool isSelected)
         {
-            for(int i = 0; i < num_menus; i++)
-            {
-                int start_x = menus[i].start_x;
-                string text = menus[i].text;
-                mvwprintw(win, 0, start_x, text.c_str());
-            }
-            wrefresh(win);
-        }
-
-        void draw()
-        {
-            for(int i = 0; i < num_menus; i++)
-            {
-                drawMenu(menus[i], selected_menu == i);
-            }
-            selected_menu = -1;
-        }
-
-        void drawMenu(Menu menu, bool is_selected)
-        {
-            
-            int start_x = menu.start_x;
-            string text = menu.text;
-            if(is_selected)
-            {
-                wattron(win, A_STANDOUT);
-            }
-            mvwprintw(win, 0, start_x, text.c_str());
-            wattroff(win, A_STANDOUT);
-            wrefresh(win);
-
-            int ch;
-            drawMenuItems(menu);
-            wrefresh(menuwin);
-            while (is_selected && (ch = wgetch(menuwin)))
-            {
-                switch(ch)
-                {
-                    case KEY_DOWN:
-                        menu.selectNextItem();
-                        break;
-                    case KEY_UP:
-                        menu.selectPreviousItem();
-                        break;
-                    default:
-                        is_selected = false;
-                }
-                drawMenuItems(menu);
-            }
-            werase(menuwin);
-            wrefresh(menuwin);
-            reset();
 
         }
 
-        void drawMenuItems(Menu menu)
+        void handleTrigger(char trigger, int numMenus)
         {
-            int yMax, xMax;
-            getmaxyx(menuwin, yMax, xMax);
-            for(int i = 0; i < menu.num_items; i++)
-            {
-                mvwprintw(menuwin, i, 0, menu.items[i].c_str());
-                if(menu.selected_item == i)
-                {
-                    mvwchgat(menuwin, i, 0, xMax, A_NORMAL, 1, NULL);
-                }
-                else
-                {
-                    mvwchgat(menuwin, i, 0, xMax, A_STANDOUT, 0, NULL);
-                }
-            }
-        }
-
-        void handleTrigger(char trigger)
-        {
-            for(int i = 0; i < num_menus; i++)
+            for(int i = 0; i < numMenus; i++)
             {
                 if(trigger == this->menus[i].trigger)
                 {
-                    selected_menu = i;
+                    
                 }
             }
-            
         }
-
-        WINDOW *menuwin;
-        WINDOW *win;
-        Menu *menus;
-        int num_menus;
-        int selected_menu;
 };
 
 #endif
