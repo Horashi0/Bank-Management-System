67d5bb9f39d856cee935f7e1950194a23f2c8d7d
Horashio <Horashi0@users.noreply.github.com>
2023-10-05 22:21:13 +0100

    Deleting code and restarting.

--- curses.hpp
+++ curses.hpp
@@ -1,4 +1,4 @@
-#include "ncurses.h"
+#include <ncurses.h>
 #include <string>
 
 using namespace std;
@@ -6,60 +6,6 @@ using namespace std;
 #ifndef __MENU_H_
 #define __MENU_H_
 
-class Menu
-{
-    public:
-        Menu(string text, char trigger)
-        {
-            this->text = text;
-            this->trigger = trigger;
-        }
 
-        string text;
-        char trigger;     
-};
-
-class MenuBar
-{
-    public:
-        MenuBar(WINDOW *win, Menu *menus, int numMenus)
-        {
-            this->win = win;
-            this->menus = menus
-        }
-
-
-
-
-
-
-
-
-        void centralText(WINDOW *win, string text, int startRowDivider)
-        {
-            int startRow = win->_maxy / startRowDivider;
-            int centerCol = win->_maxx / 2;
-            int halfLength = text.length() / 2;
-            int centralised = centerCol - halfLength;
-            
-            mvwprintw(win, startRow, centralised, text.c_str());
-        }
-
-        void draw(Menu menu, bool isSelected)
-        {
-
-        }
-
-        void handleTrigger(char trigger, int numMenus)
-        {
-            for(int i = 0; i < numMenus; i++)
-            {
-                if(trigger == this->menus[i].trigger)
-                {
-                    
-                }
-            }
-        }
-};
 
 #endif
