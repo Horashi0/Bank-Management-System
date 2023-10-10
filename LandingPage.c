6bf4c9edeb57d4959178e6fe9502ff276f030598
Horashio <Horashi0@users.noreply.github.com>
2023-10-10 23:05:29 +0100

    Fixed math formulas, renamed variables, labeled menus

--- LandingPage.c
+++ LandingPage.c
@@ -5,9 +5,9 @@
 #include <ncurses.h>
 #include <wchar.h>
 
-void printCentered(WINDOW *win, char* text);
+void printCentered(WINDOW *win, const char* text);
 void logoWinCreation(uint16_t stdscrRows, uint16_t stdscrColumns);
-void menuWinCreation(uint16_t stdscrRows, uint16_t stdscrColumns);
+void menuWinCreation(uint16_t screenHeight, uint16_t screenWidth);
 
 int main()
 {
@@ -34,34 +34,48 @@ int main()
   menuWinCreation(stdscrRows, stdscrColumns);
 }
 
-void menuWinCreation(uint16_t stdscrRows, uint16_t stdscrColumns)
+void menuWinCreation(uint16_t screenHeight, uint16_t screenWidth)
 {
-  double defaultMenuSizeRow, defaultMenuSizeColumn, defaultMenuPositionColumn;
-  double rowWidth, columnLength, positionValue;
+  double height, width, x, menuHeight, menuWidth, menuX;
 
-  defaultMenuSizeRow = 16;
-  defaultMenuSizeColumn = 1.5;
-  defaultMenuPositionColumn = defaultMenuSizeColumn * 0.125;
+  height = 0.0625; // 1/16th in decimal
+  width = 0.7;
+  x = width/2;
 
-  rowWidth = stdscrRows/defaultMenuSizeRow;
-  columnLength = stdscrColumns/defaultMenuSizeColumn;
-  positionValue = stdscrColumns*defaultMenuPositionColumn;
+  menuHeight = screenHeight*height;
+  menuWidth = screenWidth*width;
+  menuX = screenWidth*x;
+  menuX = (screenWidth - menuWidth) / 2;
 
-  WINDOW* menuWin1 = newwin(rowWidth, columnLength, stdscrRows*0.4, positionValue);
-  WINDOW* menuWin2 = newwin(rowWidth, columnLength, stdscrRows*0.5, positionValue);
-  WINDOW* menuWin3 = newwin(rowWidth, columnLength, stdscrRows*0.6, positionValue);
-  WINDOW* menuWin4 = newwin(rowWidth, columnLength, stdscrRows*0.7, positionValue);
+  WINDOW* menuWin1 = newwin(menuHeight, menuWidth, screenHeight*0.4, menuX);
+  WINDOW* menuWin2 = newwin(menuHeight, menuWidth, screenHeight*0.5, menuX);
+  WINDOW* menuWin3 = newwin(menuHeight, menuWidth, screenHeight*0.6, menuX);
+  WINDOW* menuWin4 = newwin(menuHeight, menuWidth, screenHeight*0.7, menuX);
 
   wbkgd(menuWin1, COLOR_PAIR(1));
   wbkgd(menuWin2, COLOR_PAIR(1));
   wbkgd(menuWin3, COLOR_PAIR(1));
   wbkgd(menuWin4, COLOR_PAIR(1));
 
-  wrefresh(menuWin1);
-  wrefresh(menuWin2);
-  wrefresh(menuWin3);
-  wrefresh(menuWin4);
+  const char* const menuTitles[] = {
+    "1. Create new account",
+    "2. Log into existing account",
+    "3. Update information on existing",
+    "4. Transfer money",
+  };
 
+  printCentered(menuWin1, menuTitles[0]);
+  wrefresh(menuWin1);
+
+  printCentered(menuWin2, menuTitles[1]);
+  wrefresh(menuWin2);
+
+  printCentered(menuWin3, menuTitles[2]);
+  wrefresh(menuWin3);
+
+  printCentered(menuWin4, menuTitles[3]);
+  wrefresh(menuWin4);
+  
   int ch = wgetch(menuWin4);
   while(ch)
   {
@@ -71,7 +85,7 @@ void menuWinCreation(uint16_t stdscrRows, uint16_t stdscrColumns)
 
 void logoWinCreation(uint16_t stdscrRows, uint16_t stdscrColumns)
 {
-  char title[30] = "Horashio Banking System - HBS";
+  const char* title = "Horashio Banking System - HBS";
   
   WINDOW* logoWin = newwin(stdscrRows/12, stdscrColumns/2, stdscrRows*0.1, stdscrColumns/4);
   wbkgd(logoWin, COLOR_PAIR(1));
@@ -80,7 +94,7 @@ void logoWinCreation(uint16_t stdscrRows, uint16_t stdscrColumns)
   wrefresh(logoWin);
 }
 
-void printCentered(WINDOW *win, char* text)
+void printCentered(WINDOW *win, const char* text)
 {
   uint16_t winRows, winColumns;
   uint16_t length, centerColumn, centerRow, halfLength, adjustedColumn;
@@ -92,6 +106,10 @@ void printCentered(WINDOW *win, char* text)
   centerColumn = winColumns / 2;
   centerRow = winRows / 2;
   halfLength = length / 2;
+  if(halfLength % 2 != 0)
+  {
+    halfLength++;
+  }
   adjustedColumn = centerColumn - halfLength;
 
   mvwprintw(win, centerRow, adjustedColumn, text);   
