4636182d08d13ef289ea83150e48b93dde68e7ff
Horashio <Horashi0@users.noreply.github.com>
2023-10-10 01:24:00 +0100

    Improved code readability, added 4 menu selection bars.
    Code readability improved, math expressions are variables now for improved code readability

--- LandingPage.c
+++ LandingPage.c
@@ -1,3 +1,4 @@
+#include <curses.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
@@ -20,24 +21,48 @@ int main()
       puts("Sorry, your machine isnt supported on this program.");
       endwin();
   }
-
   start_color();
+
   // Pair 1, Foreground white, Background blue
   init_pair(1, COLOR_WHITE, COLOR_BLUE);
   
   uint16_t stdscrRows, stdscrColumns;
   getmaxyx(stdscr, stdscrRows, stdscrColumns);
 
+  // Calling window creation functions
   logoWinCreation(stdscrRows, stdscrColumns);
-   
   menuWinCreation(stdscrRows, stdscrColumns);
 }
 
 void menuWinCreation(uint16_t stdscrRows, uint16_t stdscrColumns)
 {
-  WINDOW* menuWin1 = newwin(stdscrRows/8, stdscrColumns/1.5, stdscrRows/2, (stdscrColumns/6));
+  double defaultMenuSizeRow, defaultMenuSizeColumn, defaultMenuPositionColumn;
+  double rowWidth, columnLength, positionValue;
+
+  defaultMenuSizeRow = 16;
+  defaultMenuSizeColumn = 1.5;
+  defaultMenuPositionColumn = defaultMenuSizeColumn * 0.125;
+
+  rowWidth = stdscrRows/defaultMenuSizeRow;
+  columnLength = stdscrColumns/defaultMenuSizeColumn;
+  positionValue = stdscrColumns*defaultMenuPositionColumn;
+
+  WINDOW* menuWin1 = newwin(rowWidth, columnLength, stdscrRows*0.4, positionValue);
+  WINDOW* menuWin2 = newwin(rowWidth, columnLength, stdscrRows*0.5, positionValue);
+  WINDOW* menuWin3 = newwin(rowWidth, columnLength, stdscrRows*0.6, positionValue);
+  WINDOW* menuWin4 = newwin(rowWidth, columnLength, stdscrRows*0.7, positionValue);
+
   wbkgd(menuWin1, COLOR_PAIR(1));
-  int ch = wgetch(menuWin1);
+  wbkgd(menuWin2, COLOR_PAIR(1));
+  wbkgd(menuWin3, COLOR_PAIR(1));
+  wbkgd(menuWin4, COLOR_PAIR(1));
+
+  wrefresh(menuWin1);
+  wrefresh(menuWin2);
+  wrefresh(menuWin3);
+  wrefresh(menuWin4);
+
+  int ch = wgetch(menuWin4);
   while(ch)
   {
 
@@ -46,34 +71,28 @@ void menuWinCreation(uint16_t stdscrRows, uint16_t stdscrColumns)
 
 void logoWinCreation(uint16_t stdscrRows, uint16_t stdscrColumns)
 {
-  WINDOW* logoWin = newwin(stdscrRows/6, stdscrColumns/2, stdscrRows/10, stdscrColumns/4);
+  char title[30] = "Horashio Banking System - HBS";
+  
+  WINDOW* logoWin = newwin(stdscrRows/12, stdscrColumns/2, stdscrRows*0.1, stdscrColumns/4);
   wbkgd(logoWin, COLOR_PAIR(1));
-
-  uint16_t logoWinRows, logoWinColumns;
-  getmaxyx(logoWin, logoWinRows, logoWinColumns);
-
-  char title[30] = "Horashio Banking Sytem - HBS";
 
   printCentered(logoWin, title);
   wrefresh(logoWin);
-  /*int ch = wgetch(logoWin);
-  while(ch)
-  {
-  
-  }*/
 }
 
 void printCentered(WINDOW *win, char* text)
 {
   uint16_t winRows, winColumns;
+  uint16_t length, centerColumn, centerRow, halfLength, adjustedColumn;
+  
   getmaxyx(win, winRows, winColumns);
 
-  uint16_t length = strlen(text);
+  length = strlen(text);
 
-  uint16_t centerColumn = winColumns / 2;
-  uint16_t centerRow = winRows / 2;
-  uint16_t halfLength = length / 2;
-  uint16_t adjustedColumn = centerColumn - halfLength;
+  centerColumn = winColumns / 2;
+  centerRow = winRows / 2;
+  halfLength = length / 2;
+  adjustedColumn = centerColumn - halfLength;
 
   mvwprintw(win, centerRow, adjustedColumn, text);   
 }
